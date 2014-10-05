1aca8c3a7da4112d7f8355964b2a6ee6477d53bd
Jesse Xu <jessexu@JessedeMacBook-Air.local>
2014-10-05 13:36:28 -0400

    add recommendation jobs

--- app/controllers/recommendation_controller.rb
+++ app/controllers/recommendation_controller.rb
@@ -1,9 +1,18 @@
 class RecommendationController < ApplicationController
   def view
+    @categories=Array.new
+    @recjobs=Array.new
     @user=current_user
-    @jobapplications=Jobapplication.find_by_user_id(@user)
-    job=@jobapplications.job
-    @category=job.category
+    @jobapplications=@user.jobapplications
+    @jobapplications.each do |jobapplication|
+      @job=Job.find(jobapplication.job_id)
+      @categories.append(@job.field)
+    end
+    @categories.each do |category|
+      @recjobs.append(Job.find_by_field(category))
+    end
+      # job=@jobapplications.job
+    # @category=job.category
     # @category=@job.category
   end
 end
