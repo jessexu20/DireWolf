98a3a7aa5cee835bae62519e2562c7465ef3dc6e
Jesse Xu <jessexu@JessedeMacBook-Air.local>
2014-10-06 18:26:28 -0400

    fix small bugs

--- app/controllers/recommendation_controller.rb
+++ app/controllers/recommendation_controller.rb
@@ -9,22 +9,12 @@ class RecommendationController < ApplicationController
       @categories.append(@job.field)
     end
     @categories.each do |category|
-      # logger.debug category
       @reccjobs=Job.find_by_sql ["SELECT * FROM jobs WHERE field='#{category}'"]
       @reccjobs.each do |filterjob|
-        # logger.debug recjob.to_s
         if filterjob!=@job and filterjob.outdated==FALSE
         @recjobs.append(filterjob)
         end
       end
-      # logger debug @reccjob.to_s
-      #
-      #
-      # end
     end
-    logger.debug @recjobs
-      # job=@jobapplications.job
-    # @category=job.category
-    # @category=@job.category
   end
 end
