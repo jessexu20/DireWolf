3b1e79a4b8a2f5c2c8a4ae713b8f058debc51d35
Jesse <jesse.xu.20@gmail.com>
2015-04-22 14:45:50 -0400

    config simplecov plugin

--- features/support/env.rb
+++ features/support/env.rb
@@ -3,7 +3,10 @@
 # newer version of cucumber-rails. Consider adding your own code to a new file
 # instead of editing this one. Cucumber will automatically load all features/**/*.rb
 # files.
-
+require 'simplecov'
+SimpleCov.start do
+	add_filter "/features/"
+end
 require 'cucumber/rails'
 
 # Capybara defaults to CSS3 selectors rather than XPath.
